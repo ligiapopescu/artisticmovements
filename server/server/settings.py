14a32e8150286ad87993dee0ddb45925eebef606
ligiapopescu <ligia_popescu998@yahoo.ro>
2021-12-06 22:47:35 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- server/server/settings.py
+++ server/server/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get(
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['artistic-movements.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
