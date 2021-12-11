3652f827b12d4b2a2e9f0ebe52e26d184216383f
ligiapopescu <ligia_popescu998@yahoo.ro>
2021-12-11 23:32:12 +0200

    Added static folder

--- server/server/settings.py
+++ server/server/settings.py
@@ -154,6 +154,7 @@ DATABASES['default'].update(db_from_env)
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
