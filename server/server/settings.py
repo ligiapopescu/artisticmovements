55eb014d09c701c140a119d579a8ed68deaeb867
ligiapopescu <ligia_popescu998@yahoo.ro>
2021-12-17 00:31:53 +0200

    Procfile

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.localhost', 'artistic-movements.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.localhost', 'artistic-movements.herokuapp.com',
+'artisticmovements.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
